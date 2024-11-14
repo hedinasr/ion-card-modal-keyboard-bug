cc6ff1905b852c1e930d4c515d843fb8e90e3efa
hedinasr <h.nsr69@gmail.com>
2024-11-14 13:35:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ion-card-modal-keyboard-bug
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hedinasr/ion-card-modal-keyboard-bug)
---
+++ src/main.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  IonButtons,
+  IonButton,
+  IonModal,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonList,
+  IonItem,
+  IonInput,
+} from '@ionic/react';
+
+function Example() {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(null);
+
+  const [presentingElement, setPresentingElement] =
+    useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
+  return (
+    <IonPage ref={page}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonButton id="open-modal" expand="block">
+          Open
+        </IonButton>
+        <IonModal
+          ref={modal}
+          trigger="open-modal"
+          presentingElement={presentingElement!}
+        >
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Modal</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => dismiss()}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <IonList>
+              <IonItem>
+                <IonInput label="Name" />
+              </IonItem>
+            </IonList>
+          </IonContent>
+        </IonModal>
+      </IonContent>
+    </IonPage>
+  );
+}
+
+export default Example;
